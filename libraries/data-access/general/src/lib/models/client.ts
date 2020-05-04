9a166d06f7e10d40c87870cb8d1dde4cb8a05861
ianrbaguio <ianrey_2004@yahoo.com>
2020-05-03 20:18:48 -0600

    Added client model

---
+++ libraries/data-access/general/src/lib/models/client.ts
@@ -0,0 +1,10 @@
+import { ClientType } from './client-type';
+
+export class Client {
+  public ClientID: number;
+  public ClientType: ClientType;
+  public Username: string;
+  public FirstName: string;
+  public LastName: string;
+  public Email: string;
+}
