bdc9d25bc4cf77b549453f40bea1ff4817474e74
ianrbaguio <ianrey_2004@yahoo.com>
2020-07-30 20:36:24 -0600

    content added to account-summary container

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from 'src/login/login/login.component';
 import { SecurityQuestionComponent } from 'src/main/security-question/security-question.component';
+import { AccountSummaryComponent } from 'src/main/account-summary/account-summary.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: LoginComponent },
   { path: 'security-question', component: SecurityQuestionComponent },
+  { path: 'account-summary', component: AccountSummaryComponent },
 ];
 
 @NgModule({
