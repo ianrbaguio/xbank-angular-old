b61061089f732f2d2d0baf7eaef8457dba57dfce
ianrbaguio <ianrey_2004@yahoo.com>
2020-10-18 20:25:44 -0600

    Added register module & component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from 'src/login/login/login.component';
 import { SecurityQuestionComponent } from 'src/main/security-question/security-question.component';
 import { AccountSummaryComponent } from 'src/main/account-summary/account-summary.component';
+import { RegisterComponent } from 'src/register/register/register.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: LoginComponent },
   { path: 'security-question', component: SecurityQuestionComponent },
   { path: 'account-summary', component: AccountSummaryComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
