e1a842250c0562fbeb2373047c01c9e6a1e7be46
ianrbaguio <ianrey_2004@yahoo.com>
2020-10-18 21:29:58 -0600

    Added form controls for register

--- src/register/register.module.ts
+++ src/register/register.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
 import { RegisterComponent } from './register/register.component';
-
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RegisterComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class RegisterModule { }
+export class RegisterModule {}
