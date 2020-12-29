4fcfdb16d8acb150e8f35263c87d1839ecdf1eef
Ian Baguio <ianrey_2004@yahoo.com>
2020-12-28 23:15:10 -0700

    Added FormControls

--- src/register/register/register.component.ts
+++ src/register/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+
+  fNameFormControl = new FormControl();
 
   constructor() { }
 
