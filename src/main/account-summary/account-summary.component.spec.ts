7770d2adc7a9c306e99d1afb59d3782c955673ac
Ian Baguio <ianrey_2004@yahoo.com>
2020-12-28 22:42:48 -0700

    Updated @angular/core

--- src/main/account-summary/account-summary.component.spec.ts
+++ src/main/account-summary/account-summary.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AccountSummaryComponent } from './account-summary.component';
 
@@ -6,7 +6,7 @@ describe('AccountSummaryComponent', () => {
   let component: AccountSummaryComponent;
   let fixture: ComponentFixture<AccountSummaryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AccountSummaryComponent ]
     })
