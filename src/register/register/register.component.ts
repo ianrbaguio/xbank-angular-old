f5343ee19e2d7ae01bca633f48e7444829755e80
Ian Baguio <ianrey_2004@yahoo.com>
2021-02-08 19:50:41 -0700

    Added all form fields to register page

--- src/register/register/register.component.ts
+++ src/register/register/register.component.ts
@@ -8,8 +8,17 @@ import {FormControl} from "@angular/forms";
 })
 export class RegisterComponent implements OnInit {
 
+  // Personal Information Form Controls
   fNameFormControl = new FormControl();
+  lNameFormControl = new FormControl();
+  dateOfBirthFormControl = new FormControl();
+  emailFormControl = new FormControl();
 
+  // User Form Controls
+  usernameFormControl = new FormControl();
+  passwordFormControl = new FormControl();
+  savingsFormControl = new FormControl(0.00);
+  chequingFormControl = new FormControl(0.00);
   constructor() { }
 
   ngOnInit(): void {
