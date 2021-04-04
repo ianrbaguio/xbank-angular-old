615b4cb8c09ea2501a1f27b663e086199b39391c
Ian Baguio <ianrey_2004@yahoo.com>
2021-04-04 00:50:03 -0600

    Added transaction-history component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from 'src/login/login/login.component';
 import { SecurityQuestionComponent } from 'src/main/security-question/security-question.component';
 import { AccountSummaryComponent } from 'src/main/account-summary/account-summary.component';
 import { RegisterComponent } from 'src/register/register/register.component';
+import { TransactionHistoryComponent } from 'src/main/transaction-history/transaction-history.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: LoginComponent },
   { path: 'security-question', component: SecurityQuestionComponent },
   { path: 'account-summary', component: AccountSummaryComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'transaction-history', component: TransactionHistoryComponent}
 ];
 
 @NgModule({
--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 import { SecurityQuestionComponent } from './security-question/security-question.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTable, MatTableModule } from '@angular/material/table';
+import {MatSelectModule} from '@angular/material/select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AccountSummaryComponent } from './account-summary/account-summary.component';
+import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
 
 @NgModule({
-  declarations: [SecurityQuestionComponent, AccountSummaryComponent],
+  declarations: [SecurityQuestionComponent, AccountSummaryComponent, TransactionHistoryComponent],
   imports: [
     CommonModule,
     MatInputModule,
     MatButtonModule,
+    MatSelectModule,
+    MatTableModule,
     FormsModule,
     ReactiveFormsModule,
   ],
